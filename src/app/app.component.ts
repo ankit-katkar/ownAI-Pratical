2fef906519b40d10b3b8830914a8a835c9315e0f
Ankit Katkar <ankitkatkar8@.com>
2024-07-18 12:27:41 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,66 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'
+import { CommonModule } from '@angular/common';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { AccordionComponent } from "./accordion/accordion.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule, NgbAccordionModule, AccordionComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ownai-pratical';
+
+  PurchaseOrderForm:FormGroup | any;
+submitted = false;
+isDisabled = false;
+selectedFramework: string = '';
+formEnabled: boolean = false;
+
+
+
+constructor(private fb:FormBuilder){
+  this.PurchaseOrderForm = fb.group({
+    clientname:['', Validators.required],
+    purchasetype:['', Validators.required],
+    prchaseno:['', Validators.required],
+    receivedon:['', Validators.required],
+    receivedname:['', Validators.required],
+    receivedemail:['', Validators.required],
+    postartdate:['', Validators.required],
+    poenddate:['', Validators.required],
+    budget:['', Validators.required, Validators.maxLength(5)],
+    currency:['', Validators.required],
+  })
+}
+
+get f(){
+  return this.PurchaseOrderForm.controls;
+}
+
+accordions: { id: number }[] = [];
+
+addAccordion() {
+  const newId = this.accordions.length + 1;
+  this.accordions.push({ id: newId });
+}
+
+
+onreset(){
+  this.PurchaseOrderForm.enable()
+  this.PurchaseOrderForm.reset()
+}
+
+onSave(){
+  this.submitted = true
+  if(this.PurchaseOrderForm.valid){
+    this.isDisabled = true;
+    this.PurchaseOrderForm.disable()
+  }
+}
+
 }
